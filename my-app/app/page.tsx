ef42a404131e1b7cce399acfdea4669b8e9d9fb6
max <68344959+codewmax@users.noreply.github.com>
2024-10-26 16:42:39 +0200

    canvas

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -3,6 +3,9 @@
 import { useForm, SubmitHandler } from "react-hook-form";
 import { useMutation } from "convex/react";
 import { api } from "@/convex/_generated/api";
+import { ReactSketchCanvas } from "react-sketch-canvas";
+import { useRef } from "react";
+
 
 type Inputs = {
   prompt: string,
@@ -16,19 +19,38 @@ export default function Home() {
   const { register, handleSubmit, watch, formState: { errors } } = useForm<Inputs>();
   const onSubmit: SubmitHandler<Inputs> = data => console.log(data);
 
+  const canvasRef = useRef<any>(null);
+
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
 
-      <form onSubmit={handleSubmit(async (formData) => {
+
+
+
+      <form className="flex flex-col items-center justify-center gap-4" onSubmit={handleSubmit(async (formData) => {
         console.log(formData);
         const result = await saveSketchMutation({ prompt: formData.prompt });
         console.log(result);
       })}>
-        <input {...register("prompt", { required: true })} />
+        <input className="w-full" {...register("prompt", { required: true })} />
         {errors.prompt && <span>This field is required</span>}
 
-        <input type="submit" />
+
+        <ReactSketchCanvas
+          ref={canvasRef}
+          style={{
+            border: "1px solid #9c9c9c",
+            borderRadius: "5px"
+          }}
+          width="500"
+          height="500"
+          strokeWidth={5}
+          strokeColor="#000000"
+        />
+
+
+        <input className="bg-blue-500 text-white p-1 rounded-md w-full" type="submit" />
       </form>
 
     </main>
